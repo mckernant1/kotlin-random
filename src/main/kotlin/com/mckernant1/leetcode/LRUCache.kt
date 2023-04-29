d8fc61bc8475b2454895c486e2cf727a1ff2ed83
Thomas McKernan <tmeaglei@gmail.com>
2023-04-29 09:12:47 -0700

    some classes

---
+++ src/main/kotlin/com/mckernant1/leetcode/LRUCache.kt
@@ -0,0 +1,46 @@
+package com.mckernant1.leetcode
+
+
+fun main() {
+    val l = LRUCache(5)
+    l.setValue("yes", "yes")
+    l.setValue("no", "no")
+    l.setValue("hi", "hi")
+    l.setValue("bye", "bye")
+    l.setValue("wow", "wow")
+    println(l)
+    l.get("yes")
+    l.setValue("woot", "woot")
+    println(l)
+
+}
+
+class LRUCache(private val maxSize: Int) {
+
+    private val values = mutableMapOf<String, String>()
+    private val lruMap = mutableMapOf<String, Int>()
+    private var lruCounter = 0
+
+    fun setValue(key: String, value: String) {
+        if (values.size >= maxSize) {
+            val keyToRemove = lruMap.entries.minByOrNull { it.value }!!.key
+            lruMap.remove(keyToRemove)
+            values.remove(keyToRemove)
+        }
+        values[key] = value
+        lruMap[key] = lruCounter
+        ++lruCounter
+    }
+
+    fun get(key: String): String? {
+        val toReturn = values[key]
+        if (toReturn != null) {
+            lruMap[key] = lruCounter
+            ++lruCounter
+        }
+        return toReturn
+    }
+
+    override fun toString(): String = values.toString()
+
+}
---
+++ src/main/kotlin/com/mckernant1/leetcode/RotatingArray.kt
@@ -0,0 +1,49 @@
+package com.mckernant1.leetcode
+
+
+fun main() {
+    val r = RotatingArray(5)
+    r.add(1)
+    r.add(2)
+    r.add(3)
+    r.add(4)
+    r.add(5)
+    println(r)
+
+    r.add(6)
+    r.add(7)
+    println(r)
+
+    r.remove()
+    r.add(8)
+    r.add(9)
+
+    println(r)
+}
+
+class RotatingArray(val size: Int) {
+
+    private val arr = Array(size) { 0 }
+
+     var start = 0
+     var end = 0
+
+    fun add(t: Int) {
+        arr[end] = t
+
+        if (end >= size - 1) {
+            end = 0
+        } else {
+            ++end
+        }
+    }
+
+    fun remove(): Int {
+        val ret = arr[start]
+        arr[start] = 0
+        ++start
+        return ret
+    }
+
+    override fun toString(): String = arr.toList().toString()
+}
